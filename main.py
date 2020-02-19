e85de5007768e2f577494fa5ea2ba1dab04029ad
Meris Čulić <meris.culic@gmail.com>
2020-02-19 23:11:54 +0100

    Add session token and password hash function

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, make_response
+import uuid
+import hashlib
 from models import db, User
 
 app = Flask(__name__)
@@ -7,13 +9,12 @@ db.create_all()
 
 @app.route("/", methods=["GET"])
 def index():
-    email_address = request.cookies.get("email")
+    session_token = request.cookies.get("session_token")
 
-    if email_address:
-        user = db.query(User).filter_by(email=email_address).first()
+    if session_token:
+        user = db.query(User).filter_by(session_token=session_token).first()
     else:
         user = None
-
 
     return render_template("index.html", user=user)
 
@@ -24,17 +25,21 @@ def login():
     email = request.form.get("user-email")
     password = request.form.get("user-password")
 
-    user = User(name=name, email=email, password=password)
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    user = User(name=name, email=email, password=hashed_password)
 
-    db.add(user)
-    db.commit()
-
-    if password != user.password:
+    if hashed_password != user.password:
         return "WRONG PASSWORD! Go back and try again."
+    elif hashed_password==user.password:
+        session_token = str(uuid.uuid4())
 
-    response = make_response(redirect(url_for('index')))
-    response.set_cookie("email", email)
-    return response
+        user.session_token = session_token
+        db.add(user)
+        db.commit()
+
+        response = make_response(redirect(url_for('index')))
+        response.set_cookie("session_token", session_token, httponly=True, samesite='Strict')
+        return response
 
 
 if __name__ == "__main__":
